2e07634eb38130788a375d91fe53c53cc176e62b
Clément Lagier <clement.lagier@epitech.eu>
2025-05-08 01:32:47 +0900

    [ADD] Added some error constants and response template

--- include/Server.hpp
+++ include/Server.hpp
@@ -9,8 +9,20 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <exception>
+#include <sstream>
 
 #define WRONG_PORT_ERROR "Port number must be between 1024 and 65535"
+#define SOCKET_ERROR "Socket creation failed"
+#define BIND_ERROR "Binding failed"
+#define LISTEN_ERROR "Listening failed"
+#define ACCEPT_ERROR "Accepting connection failed"
+#define RECV_ERROR "Receiving data failed"
+#define SEND_ERROR "Sending data failed"
+
+#define RESPONSE_OK "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n"
+#define RESPONSE_NOT_FOUND "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"
+#define RESPONSE_BAD_REQUEST "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n"
+#define RESPONSE_INTERNAL_SERVER_ERROR "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\n\r\n"
 
 class Server {
   public:
@@ -33,6 +45,7 @@ class Server {
     void handleClient(int clientSocket);
     void sendResponse(int clientSocket, const std::string& response);
     std::string receiveRequest(int clientSocket);
+    std::string processRequest(const std::string& request);
 
     std::string _host;
     unsigned short _port;
